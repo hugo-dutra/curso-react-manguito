f8cef87ad8f7ede5b71e14831f854b693c178590
hugo-dutra <hugo.dutra@hotmail.com>
2022-04-04 14:34:57 -0300

    feat: ensure AxiosHttpCliente calls axios withcorrect body

--- src/infra/http/axios-http-client/axios-http.client.spec.ts
+++ src/infra/http/axios-http-client/axios-http.client.spec.ts
@@ -14,16 +14,10 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and Verb', async () => {
-    const request = mockPostRequest();
-    const sut = makeSut();
-    await sut.post(request);
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
-  })
-  test('Should call axios with correct body', async () => {
+  test('Should call axios with correct values', async () => {
     const request = mockPostRequest()
     const sut = makeSut();
     await sut.post(request);
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
